87d1d809c1219c453cc7adcf47916c85a8b3f827
M.D.Ward <dev@mattdw.co.uk>
2016-04-24 09:33:21 +0100

    Added and configured controller skeleton for user activities.

---
+++ src/RemainFriendsEu/Api/Bundle/RestApiBundle/Controller/UserActivityController.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * UserActivityController.php
+ * Definition of class UserActivityController
+ * 
+ * Created 24-Apr-2016 09:13:02
+ *
+ * @author M.D.Ward <dev@mattdw.co.uk>
+ * Copyright (c) 2016, 
+ */
+
+namespace RemainFriendsEu\Api\Bundle\RestApiBundle\Controller;
+
+/**
+ * UserActivityController
+ *
+ * @author M.D.Ward <dev@mattdw.co.uk>
+ */
+class UserActivityController
+{
+    
+    public function __construct(
+        
+    ) {
+        
+    }
+    
+}
